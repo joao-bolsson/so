eb6ebe9db3ff5b80f125947ebff8b082c8e09800
João Bolsson <joaovictorbolsson@gmail.com>
2019-04-23 21:49:37 -0300

    tentativa para criacao de forks

--- t2/main.c
+++ t2/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <wait.h>
 
 void listdir(const char *name, int indent) {
     DIR *dir;
@@ -10,21 +13,37 @@ void listdir(const char *name, int indent) {
     dir = opendir(name);
     if (dir == NULL) return;
 
+    int estado;
+    pid_t idProcesso;
+    
     while ((entry = readdir(dir))) {
         if (entry->d_type == DT_DIR) {
             char path[1024];
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
-            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
-            listdir(path, indent + 2);
-        } else {
-            printf("%*s- %s\n", indent, "", entry->d_name);
+
+            idProcesso = fork();
+            wait(&estado);
+
+            if (idProcesso < 0) {
+                fprintf(stderr, "fork falhou\n");
+                closedir(dir);
+                exit(-1);
+            }
+
+            if (idProcesso == 0) { //filho
+                snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+
+                printf("%d -> %d;\n", getppid(), getpid());
+                printf("%d [label=\"%s, %d\"];\n", getpid(), name, getpid());
+                listdir(path, indent + 2);
+            }
         }
     }
     closedir(dir);
 }
 
 int main() {
-    listdir("./../home/", 0);
+    listdir("./home/estagiario", 0);
+
     return 0;
 }
