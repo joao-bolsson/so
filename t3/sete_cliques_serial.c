eb3fdca9cb8f53463705472eceb666cd69201bf4
João Bolsson <joaovictorbolsson@gmail.com>
2019-05-21 00:52:57 -0300

    inicio de estruturas para pilha de paginas a serem lidas

--- t3/sete_cliques_serial.c
+++ t3/sete_cliques_serial.c
@@ -4,34 +4,69 @@
  */
 #include "curl_code.h"
 
+#define LINKS 50
+#define CLICKS_COUNT 7
+
+typedef struct {
+    /**
+     * Link name.
+     */
+    char *link;
+
+    /**
+     * Page content of the link.
+     */
+    char *pageContent;
+} LINK;
+
+typedef struct {
+    /**
+     * All the links in a page.
+     */
+    LINK links[LINKS];
+} PAGE;
+
+typedef struct {
+    /**
+     * Pages of the stack.
+     */
+    PAGE pages[CLICKS_COUNT];
+} STACK;
+
 int main() {
     char *word = "Sport Club Internacional";
 
     srand(time(NULL));
     CURL *curl_handle;
 
-    // page_content recebe o conteudo da pagina de partida: Sistema_operativo
-    char *page_content = download_page(curl_handle, "https://pt.wikipedia.org/wiki/Sistema_operativo");
+    LINK first= {.link = "https://pt.wikipedia.org/wiki/Sistema_operativo"};
+    first.pageContent = download_page(curl_handle, first.link);
 
-    printf("%s\n", page_content);
+    PAGE firstPage = {};
 
     // links_readed: variavel onde o numero de links lidos eh salvo.
     int links_readed;
-    // links: lista de links lidos. A funcao find_links ira tentar ler 50
-    // links dentro da pagina.
-    char **links = find_links(curl_handle, page_content, 50, &links_readed);
+    // links inside the page. that will get 50 links
+    char **links = find_links(curl_handle, first.pageContent, LINKS, &links_readed);
 
-    if (links_readed == 50) {
-        // Mostra os links
-        for (int i = 0; i < 50; i++) {
+    if (links_readed == LINKS) {
+        // show all links
+        for (int i = 0; i < LINKS; i++) {
+            LINK link = {.link = links[i]};
+            firstPage.links[i] = link;
+
             printf("%d. %s\n", i, links[i]);
         }
     }
 
+    STACK stack;
+    stack.pages[0] = firstPage;
+
+
     // Sorteia um dos links para ser acessado.
     int random = rand() % links_readed;
 
-    printf("\nLINK SORTEADO: %s\n", links[random]);
+    printf("\nLINK SORTEADO: %s\n", firstPage.links[random].link);
 
     /* we're done with libcurl, so clean it up */
     curl_global_cleanup();
